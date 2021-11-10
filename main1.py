2f8cf5bfc4ba39fec7fa82912ac78a9c641157e5
Teja Sri <nimmaditeja@gmail.com>
2021-11-10 10:31:54 +0530

    print statement was updated

--- main1.py
+++ main1.py
@@ -4,6 +4,6 @@ p=n**(1/2)
 for i in range(2):
                 m=n/p
 if(m==p):
-            print("Perfect sqaure")
+            print("Perfect square Number")
 else:
-     print("no")
+     print("Not perfect square Number")
